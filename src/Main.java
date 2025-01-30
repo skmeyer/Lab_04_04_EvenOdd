2a4d443bfe86ed3613f3e59c257150f1edee7c54
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-01-30 17:23:10 -0500

    Complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        int numToExamine = 37;
+        int evenOrOdd = numToExamine%2;
+
+        System.out.println("If the following number is a 0, the number is even. If it's a 1, it's odd: " + evenOrOdd);
+        System.out.println("Since the number was a 1, " + numToExamine + " is an odd number!");
+
+
     }
 }
